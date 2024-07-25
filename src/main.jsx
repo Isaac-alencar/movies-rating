9b85949e3a20069a318194c7c2ff8965395b75df
Isaac Alencar <alencarisaac3@gmail.com>
2024-07-25 17:56:05 -0300

    add react router dom
    define the root path for application

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
 
 import "../src/styles/reset.scss";
 import "@fontsource/roboto";
 
+import { router } from "./routes";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} fallbackElement={<h1>Loading...</h1>} />
   </React.StrictMode>
 );
